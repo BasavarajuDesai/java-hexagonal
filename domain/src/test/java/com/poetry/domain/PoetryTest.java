1e2a258a06e4b96863a4950f0274b5e79599b3ad
Paul <paul58914080@gmail.com>
2019-01-24 06:35:50 +0530

    KATA-1(documentation) - Added documentation to make it more clear

--- domain/src/test/java/com/poetry/domain/PoetryTest.java
+++ domain/src/test/java/com/poetry/domain/PoetryTest.java
@@ -13,8 +13,12 @@ public class PoetryTest {
     @Test
     @DisplayName("Should be able to get verses when asked for poetry")
     public void should_give_verses_when_asked_for_poetry() {
-        RequestVerse poertyReader = new PoetryReader();
-        String verses = poertyReader.giveMeSomePoetry();
+        /*
+            RequestVerse - left side port
+            PoetryReader - hexagon (domain)
+         */
+        RequestVerse poetryReader = new PoetryReader();
+        String verses = poetryReader.giveMeSomePoetry();
         Assertions.assertEquals("If you could read a leaf or tree\\r\\nyou’d have no need of books.\\r\\n-- © Alistair Cockburn (1987)", verses);
     }
 }
